bb8dfd4d867a21714115e4283c6f900caed47074
Sermob112 <motherboard879@gmail.com>
2022-11-21 16:00:51 +0300

    Робачая группа админа и пользователя

--- Try3/Controllers/HomeController.cs
+++ Try3/Controllers/HomeController.cs
@@ -1,18 +1,28 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity.Owin;
+using Microsoft.AspNet.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Try3.Models;
 
 namespace Try3.Controllers
 {
     public class HomeController : Controller
     {
+        [Authorize]
         public ActionResult Index()
         {
-            return View();
+            IList<string> roles = new List<string> { "Роль не определена" };
+            ApplicationUserManager userManager = HttpContext.GetOwinContext()
+                                                    .GetUserManager<ApplicationUserManager>();
+            ApplicationUser user = userManager.FindByEmail(User.Identity.Name);
+            if (user != null)
+                roles = userManager.GetRoles(user.Id);
+            return View(roles);
         }
-
+        [Authorize(Roles = "admin")]
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
--- Try3/Global.asax.cs
+++ Try3/Global.asax.cs
@@ -1,10 +1,12 @@
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using Try3.Models;
 
 namespace Try3
 {
@@ -12,6 +14,8 @@ namespace Try3
     {
         protected void Application_Start()
         {
+            //закоментить после первого вызова
+            Database.SetInitializer<ApplicationDbContext>(new AppDbInitializer());
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
